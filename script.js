9deced3915ac271a17fc514a1cb463c2fc9cef01
jcLucio <c.lucio3@gmail.com>
2020-02-13 11:42:59 -0600

    Add operator functions and button functionality

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 // Javascript for PROJECT: CALCULATOR
+const output = document.querySelector('#output')
+const btns = document.querySelectorAll('button');
+const acBtn = document.querySelector('#ac');
+const backspaceBtn = document.querySelector('#backspace');
+const pointBtn = document.querySelector('#point');
+
+function add(a, b) {
+	return a + b;
+}
+function subtract(a, b) {
+	return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a/b;
+}
+
+btns.forEach((btn) => {
+    if (!isNaN(parseInt(btn.textContent))) {
+        btn.addEventListener('click', (e) => {
+            output.textContent += btn.textContent;
+        })
+    } else if (btn.textContent === '.') {
+        btn.addEventListener('click', (e) => {
+            output.textContent += btn.textContent;
+            pointBtn.disabled = true;
+        })
+    }
+})
+acBtn.addEventListener('click', (e) => {
+    output.textContent = '';
+    pointBtn.disabled = false;
+})
+backspaceBtn.addEventListener('click', (e)=>{
+    if (output.textContent.slice(-1) === '.'){
+        output.textContent = output.textContent.slice(-output.textContent.length, -1);
+        pointBtn.disabled = false;
+    } else {
+        output.textContent = output.textContent.slice(-output.textContent.length, -1);
+    }
+})
