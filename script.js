c23dde20014dda84c0d85796f4a44bbbe7999791
jcLucio <c.lucio3@gmail.com>
2020-02-14 22:50:36 -0600

    Add operators functionality and refactor code

--- script.js
+++ script.js
@@ -1,44 +1,73 @@
 // Javascript for PROJECT: CALCULATOR
-const output = document.querySelector('#output')
+const output = document.querySelector('#output');
 const btns = document.querySelectorAll('button');
-const acBtn = document.querySelector('#ac');
-const backspaceBtn = document.querySelector('#backspace');
 const pointBtn = document.querySelector('#point');
+let equation = [];
+let result = 0;
+let i = 0;
 
 function add(a, b) {
-	return a + b;
+  return a + b;
 }
 function subtract(a, b) {
-	return a - b;
+  return a - b;
 }
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
-function divide(a, b){
-    return a/b;
+function divide(a, b) {
+  return a / b;
+}
+function clearOutput() {
+  output.textContent = '';
+  pointBtn.disabled = false;
+}
+function operate() {
+  while (equation.includes('X')) {
+    i = equation.indexOf('X');
+    result = multiply(equation[i - 1], equation[i + 1]);
+    equation.splice(i - 1, 3, result);
+  }
+  while (equation.includes('÷')) {
+    i = equation.indexOf('÷');
+    result = divide(equation[i - 1], equation[i + 1]);
+    equation.splice(i - 1, 3, result);
+  }
+  while (equation.includes('-')) {
+    i = equation.indexOf('-');
+    result = subtract(equation[i - 1], equation[i + 1]);
+    equation.splice(i - 1, 3, result);
+  }
+  while (equation.includes('+')) {
+    i = equation.indexOf('+');
+    result = add(equation[i - 1], equation[i + 1]);
+    equation.splice(i - 1, 3, result);
+  }
 }
 
 btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
     if (!isNaN(parseInt(btn.textContent))) {
-        btn.addEventListener('click', (e) => {
-            output.textContent += btn.textContent;
-        })
+      output.textContent += btn.textContent;
     } else if (btn.textContent === '.') {
-        btn.addEventListener('click', (e) => {
-            output.textContent += btn.textContent;
-            pointBtn.disabled = true;
-        })
-    }
-})
-acBtn.addEventListener('click', (e) => {
-    output.textContent = '';
-    pointBtn.disabled = false;
-})
-backspaceBtn.addEventListener('click', (e)=>{
-    if (output.textContent.slice(-1) === '.'){
-        output.textContent = output.textContent.slice(-output.textContent.length, -1);
-        pointBtn.disabled = false;
+        output.textContent += btn.textContent;
+        pointBtn.disabled = true;
+    } else if (btn.textContent === 'AC') {
+        clearOutput();
+        equation = [];
+    } else if (btn.textContent === '<--') {
+      output.textContent = output.textContent.slice(-output.textContent.length, -1);
+        if (output.textContent.slice(-1) === '.') {
+          pointBtn.disabled = false;
+        } 
+    } else if (btn.textContent === '=') {
+        equation[equation.length] = Number(output.textContent);
+        operate();
+        output.textContent = result;
     } else {
-        output.textContent = output.textContent.slice(-output.textContent.length, -1);
+        equation[equation.length] = Number(output.textContent);
+        equation[equation.length] = btn.textContent;
+        clearOutput();
     }
+  })
 })
