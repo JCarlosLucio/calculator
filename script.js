4e665dfc4c6192fd2b946cf4e06aeba8866471e7
jcLucio <c.lucio3@gmail.com>
2020-02-15 23:53:55 -0600

    Refactor operate function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const backspaceBtn = document.querySelector('#backspace');
 const equalBtn = document.querySelector('#equal');
 const output = document.querySelector('#output');
 const pointBtn = document.querySelector('#point');
+const operators = ['*', '/', '-', '+'];
 let equation = [];
 let i = 0;
 let result = 0;
@@ -24,7 +25,7 @@ function clearOutput() {
   output.textContent = '';
   pointBtn.disabled = false;
 }
-function btnsAction(btn){
+function btnsAction(btn) {
   if (!isNaN(parseInt(btn.textContent))) {
     output.textContent += btn.textContent;
   } else if (btn.textContent === '.') {
@@ -43,38 +44,33 @@ function btnsAction(btn){
     clearOutput();
   }
 }
-function equalBtnAction(){
+function equalBtnAction() {
   equation[equation.length] = Number(output.textContent);
   operate();
-  output.textContent = Math.round((result + Number.EPSILON)*10000)/10000; //Rounds number to 4 decimal places
+  output.textContent = Math.round((result + Number.EPSILON) * 10000) / 10000; //Rounds number to 4 decimal places
 }
-function backspaceBtnAction(){
+function backspaceBtnAction() {
   output.textContent = output.textContent.slice(-output.textContent.length, -1);
   if (output.textContent.slice(-1) === '.') {
     pointBtn.disabled = false;
   }
 }
 function operate() {
-  while (equation.includes('*')) {
-    i = equation.indexOf('*');
-    result = multiply(equation[i - 1], equation[i + 1]);
-    equation.splice(i - 1, 3, result);
-  }
-  while (equation.includes('/')) {
-    i = equation.indexOf('/');
-    result = divide(equation[i - 1], equation[i + 1]);
-    equation.splice(i - 1, 3, result);
-  }
-  while (equation.includes('-')) {
-    i = equation.indexOf('-');
-    result = subtract(equation[i - 1], equation[i + 1]);
-    equation.splice(i - 1, 3, result);
-  }
-  while (equation.includes('+')) {
-    i = equation.indexOf('+');
-    result = add(equation[i - 1], equation[i + 1]);
-    equation.splice(i - 1, 3, result);
-  }
+  operators.forEach((op) => {
+    while (equation.includes(op)){
+      i = equation.indexOf(op);
+      if (op === '*') {
+        result = multiply(equation[i - 1], equation[i + 1]);
+      } else if (op === '/') {
+        result = divide(equation[i - 1], equation[i + 1]);
+      } else if (op === '-') {
+        result = subtract(equation[i - 1], equation[i + 1]);
+      } else if (op === '+') {
+        result = add(equation[i - 1], equation[i + 1]);
+      }
+      equation.splice(i - 1, 3, result);
+    }
+  })
 }
 
 btns.forEach((btn) => {
