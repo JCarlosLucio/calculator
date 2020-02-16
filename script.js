09e1fa84824a550e604f45cee08de4d91020326b
jcLucio <c.lucio3@gmail.com>
2020-02-15 22:19:05 -0600

    Add keyboard support

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Javascript for PROJECT: CALCULATOR
-const output = document.querySelector('#output');
 const btns = document.querySelectorAll('button');
 const pointBtn = document.querySelector('#point');
+const backspaceBtn = document.querySelector('#backspace');
+const equalBtn = document.querySelector('#equal');
+const output = document.querySelector('#output');
 let equation = [];
 let result = 0;
 let i = 0;
@@ -22,14 +24,25 @@ function clearOutput() {
   output.textContent = '';
   pointBtn.disabled = false;
 }
+function equalBtnAction(){
+  equation[equation.length] = Number(output.textContent);
+  operate();
+  output.textContent = result;
+}
+function backspaceBtnAction(){
+  output.textContent = output.textContent.slice(-output.textContent.length, -1);
+  if (output.textContent.slice(-1) === '.') {
+    pointBtn.disabled = false;
+  }
+}
 function operate() {
-  while (equation.includes('X')) {
-    i = equation.indexOf('X');
+  while (equation.includes('*')) {
+    i = equation.indexOf('*');
     result = multiply(equation[i - 1], equation[i + 1]);
     equation.splice(i - 1, 3, result);
   }
-  while (equation.includes('÷')) {
-    i = equation.indexOf('÷');
+  while (equation.includes('/')) {
+    i = equation.indexOf('/');
     result = divide(equation[i - 1], equation[i + 1]);
     equation.splice(i - 1, 3, result);
   }
@@ -50,24 +63,60 @@ btns.forEach((btn) => {
     if (!isNaN(parseInt(btn.textContent))) {
       output.textContent += btn.textContent;
     } else if (btn.textContent === '.') {
+      output.textContent += btn.textContent;
+      pointBtn.disabled = true;
+    } else if (btn.textContent === 'AC') {
+      clearOutput();
+      equation = [];
+    } else if (btn.textContent === '<--') {
+      backspaceBtnAction()
+    } else if (btn.textContent === '=') {
+      equalBtnAction();
+    } else {
+      equation[equation.length] = Number(output.textContent);
+      equation[equation.length] = btn.textContent;
+      clearOutput();
+    }
+  })
+})
+document.addEventListener('keydown', (e) => {
+  btns.forEach((btn) => {
+    if (btn.textContent === e.key) {
+      btn.classList.toggle('pressed');
+      if (!isNaN(parseInt(btn.textContent))) {
+        output.textContent += btn.textContent;
+      } else if (btn.textContent === '.') {
         output.textContent += btn.textContent;
         pointBtn.disabled = true;
-    } else if (btn.textContent === 'AC') {
+      } else if (btn.textContent === 'AC') {
         clearOutput();
         equation = [];
-    } else if (btn.textContent === '<--') {
-      output.textContent = output.textContent.slice(-output.textContent.length, -1);
-        if (output.textContent.slice(-1) === '.') {
-          pointBtn.disabled = false;
-        } 
-    } else if (btn.textContent === '=') {
-        equation[equation.length] = Number(output.textContent);
-        operate();
-        output.textContent = result;
-    } else {
+      } else if (btn.textContent === '=') {
+        equalBtnAction()
+      } else {
         equation[equation.length] = Number(output.textContent);
         equation[equation.length] = btn.textContent;
         clearOutput();
+      }
     }
   })
+  if (e.key === 'Backspace') {
+    backspaceBtn.classList.toggle('pressed')
+    backspaceBtnAction();
+  } else if (e.key === 'Enter') {
+    equalBtn.classList.toggle('pressed');
+    equalBtnAction();
+  }
+})
+document.addEventListener('keyup', (e) => {
+  btns.forEach((btn) => {
+    if (btn.textContent === e.key) {
+      btn.classList.toggle('pressed');
+    }
+  })
+  if (e.key === 'Backspace') {
+    backspaceBtn.classList.toggle('pressed');
+  } else if (e.key === 'Enter') {
+    equalBtn.classList.toggle('pressed');
+  }
 })
