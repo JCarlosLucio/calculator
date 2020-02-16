9cd767e7ccb74b8b2d7b3df0050b0a4fb22e0cba
jcLucio <c.lucio3@gmail.com>
2020-02-15 23:11:20 -0600

    Add btnsAction() to refactor code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Javascript for PROJECT: CALCULATOR
 const btns = document.querySelectorAll('button');
-const pointBtn = document.querySelector('#point');
 const backspaceBtn = document.querySelector('#backspace');
 const equalBtn = document.querySelector('#equal');
 const output = document.querySelector('#output');
+const pointBtn = document.querySelector('#point');
 let equation = [];
-let result = 0;
 let i = 0;
+let result = 0;
 
 function add(a, b) {
   return a + b;
@@ -23,6 +23,25 @@ function divide(a, b) {
 function clearOutput() {
   output.textContent = '';
   pointBtn.disabled = false;
+}
+function btnsAction(btn){
+  if (!isNaN(parseInt(btn.textContent))) {
+    output.textContent += btn.textContent;
+  } else if (btn.textContent === '.') {
+    output.textContent += btn.textContent;
+    pointBtn.disabled = true;
+  } else if (btn.textContent === 'AC') {
+    clearOutput();
+    equation = [];
+  } else if (btn.textContent === '<--') {
+    backspaceBtnAction()
+  } else if (btn.textContent === '=') {
+    equalBtnAction()
+  } else {
+    equation[equation.length] = Number(output.textContent);
+    equation[equation.length] = btn.textContent;
+    clearOutput();
+  }
 }
 function equalBtnAction(){
   equation[equation.length] = Number(output.textContent);
@@ -60,46 +79,17 @@ function operate() {
 
 btns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    if (!isNaN(parseInt(btn.textContent))) {
-      output.textContent += btn.textContent;
-    } else if (btn.textContent === '.') {
-      output.textContent += btn.textContent;
-      pointBtn.disabled = true;
-    } else if (btn.textContent === 'AC') {
-      clearOutput();
-      equation = [];
-    } else if (btn.textContent === '<--') {
-      backspaceBtnAction()
-    } else if (btn.textContent === '=') {
-      equalBtnAction();
-    } else {
-      equation[equation.length] = Number(output.textContent);
-      equation[equation.length] = btn.textContent;
-      clearOutput();
-    }
+    btnsAction(btn);
   })
 })
 document.addEventListener('keydown', (e) => {
   btns.forEach((btn) => {
     if (btn.textContent === e.key) {
       btn.classList.toggle('pressed');
-      if (!isNaN(parseInt(btn.textContent))) {
-        output.textContent += btn.textContent;
-      } else if (btn.textContent === '.') {
-        output.textContent += btn.textContent;
-        pointBtn.disabled = true;
-      } else if (btn.textContent === 'AC') {
-        clearOutput();
-        equation = [];
-      } else if (btn.textContent === '=') {
-        equalBtnAction()
-      } else {
-        equation[equation.length] = Number(output.textContent);
-        equation[equation.length] = btn.textContent;
-        clearOutput();
-      }
+      btnsAction(btn);
     }
   })
+  //Ifs to account for buttons with textContent that isn't the same as .key
   if (e.key === 'Backspace') {
     backspaceBtn.classList.toggle('pressed')
     backspaceBtnAction();
